9ae8e9e2a89a95422be35f50a53570cfa5a6745e
Nikolas Pereira Accampora Neto <89613850+NikoAccampora@users.noreply.github.com>
2021-11-18 16:53:55 -0300

    myupd

--- MegaMan_FanSite/src/main/java/com/nikoaccampora/repository/CharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nikoaccampora.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nikoaccampora.model.Character;
-
-public interface CharacterRepository extends CrudRepository<Character, Integer> {
-
-}
